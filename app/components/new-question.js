3d8eb4e7b04b7d57268bda431489c391e4fc2a61
Mark Lawson <mlawson3691@gmail.com>
2016-10-28 09:48:34 -0700

    reformat new-question component

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newQuestion: false,
+
   actions: {
+    newQuestion() {
+      this.set('newQuestion', true);
+    },
+    cancelQuestion() {
+      this.set('newQuestion', false);
+    },
     saveQuestion() {
       var params = {
         content: this.get('content'),
@@ -11,6 +19,7 @@ export default Ember.Component.extend({
       this.set('content', '');
       this.set('notes', '');
       this.set('author', '');
+        this.set('newQuestion', false);
       this.sendAction('saveQuestion', params);
     }
   }
