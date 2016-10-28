4af79c0980b2aa1024c83c3e76cd7e4836d48af5
Mark Lawson <mlawson3691@gmail.com>
2016-10-28 11:38:26 -0700

    add answerCount property to question model, and display this on question-tile

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,9 @@ export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
   notes: DS.attr(),
-  answers: DS.hasMany('answer', { asyng: true })
+  answers: DS.hasMany('answer', { async: true }),
+
+  answerCount: Ember.computed('answers', function() {
+    return this.get('answers').toArray().length;
+  })
 });
