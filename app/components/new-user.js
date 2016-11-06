7f0501270cb579810bff08621add26fc95ce2410
Mark Lawson <mlawson3691@gmail.com>
2016-11-06 09:01:08 -0800

    prevent users from signing up with duplicate usernames

--- app/components/new-user.js
+++ app/components/new-user.js
@@ -1,22 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   error: false,
+  duplicateError: false,
   actions: {
     signUp() {
-      if (this.get('newUsername') !== undefined && this.get('newUsername') !== "" && this.get('newPassword') !== undefined && this.get('newPassword') !== "") {
-        var params = {
-          username: this.get('newUsername'),
-          password: this.get('newPassword')
+      var _this = this;
+      this.get('store').query('user', {
+        orderBy: 'username',
+        equalTo: this.get('newUsername')
+      }).then(function(result) {
+        if (result.content[0]) {
+          _this.set('duplicateError', true);
+        } else {
+          _this.set('duplicateError', false);
         }
-        this.set('newUsername', '');
-        this.set('newPassword', '');
-        this.sendAction('signUp', params);
-        this.$('#signUpModal').modal('toggle');
-        this.set('error', false);
-      } else {
-        this.set('error', true);
-      }
+      }).then(function() {
+        if (_this.get('newUsername') !== undefined && _this.get('newUsername') !== "" && _this.get('newPassword') !== undefined && _this.get('newPassword') !== "") {
+          if (!_this.get('duplicateError')) {
+            var params = {
+              username: _this.get('newUsername'),
+              password: _this.get('newPassword')
+            };
+            _this.sendAction('signUp', params);
+            _this.$('#signUpModal').modal('toggle');
+          }
+          _this.set('newUsername', '');
+          _this.set('newPassword', '');
+          _this.set('error', false);
+        } else {
+          _this.set('error', true);
+        }
+      });
     }
   }
 });
